df94959b741eee4c9ec8274f926a3b67b463d858
ADOU Kouamé Mathurin <82063354+AKMADOU@users.noreply.github.com>
2024-03-01 18:17:51 +0000

    Update txt.py

--- txt.py
+++ txt.py
@@ -1,110 +1,3 @@
-# import json
-# def questions_to_json(file_path):
-#     json_structure = []
-
-#     with open(file_path, "r", encoding="utf-8") as file:
-#         question_text = ""
-#         for line in file:
-#             if line.strip():  # Vérifier si la ligne n'est pas vide
-#                 question_text += line
-#             else:
-#                 # Si la ligne est vide, traiter la question actuelle
-#                 if question_text:
-#                     question_dict = parse_question(question_text)
-#                     json_structure.append(question_dict)
-#                     question_text = ""
-
-#         # Traiter la dernière question si le fichier ne se termine pas par une ligne vide
-#         if question_text:
-#             question_dict = parse_question(question_text)
-#             json_structure.append(question_dict)
-
-#     return json.dumps(json_structure, indent=4, ensure_ascii=False)
-
-# def parse_question(question_text):
-#     # Séparer la question et les choix
-#     parts = question_text.split("\n")
-#     question = parts[0].strip()
-#     choices = [part.strip() for part in parts[1:] if part.strip()]
-
-#     # Extraire la réponse
-#     if choices:
-#         answer_part = choices.pop(-1).split(":")
-#         if len(answer_part) > 1:
-#             answer = answer_part[-1].strip()
-#         else:
-#             answer = "Pas de réponse fournie"
-#     else:
-#         answer = "Pas de réponse fournie"
-
-#     # Formater les choix
-#     formatted_choices = [choice.replace(answer, answer.strip(), 1) for choice in choices]
-
-#     return {
-#         "question": question,
-#         "choices": formatted_choices,
-#         "answer": answer
-#     }
-
-
-# # Chemin du fichier contenant les questions
-# input_file_path = "quiz.txt"
-
-# # Convertir les questions en format JSON
-# json_result = questions_to_json(input_file_path)
-
-# # Écriture du contenu JSON dans un fichier
-# output_file_path = "questions100.json"
-# with open(output_file_path, "w", encoding="utf-8") as json_file:
-#     json_file.write(json_result)
-
-# print("Le fichier JSON a été créé avec succès.")
-
-# from fastapi import FastAPI
-
-# # Créer une instance de l'application FastAPI
-# app = FastAPI()
-
-# # Définir une route avec un seul paramètre en tant que chaîne de caractères
-# @app.get("/theme/{theme_name}")
-# async def get_theme(theme_name: str):
-#     return {"theme_name": theme_name}
-
-# from fastapi import FastAPI
-# import os
-
-# # Créer une instance de l'application FastAPI
-# app = FastAPI()
-
-# # Chemin du fichier pour stocker theme_name
-# file_path = "theme.txt"
-
-# # Fonction pour stocker theme_name dans un fichier
-# def store_theme_name(theme_name: str):
-#     with open(file_path, "w") as file:
-#         file.write(theme_name)
-
-# # Fonction pour récupérer theme_name depuis un fichier
-# def get_theme_name():
-#     if os.path.exists(file_path):
-#         with open(file_path, "r") as file:
-#             return file.read()
-#     else:
-#         return None
-
-# # Définir une route avec un seul paramètre en tant que chaîne de caractères
-# @app.get("/theme/{theme_name}")
-# async def get_theme(theme_name: str):
-#     # Stocker le theme_name
-#     store_theme_name(theme_name)
-#     return {"theme_name": theme_name}
-
-# # Route pour récupérer le theme_name stocké
-# @app.get("/current_theme")
-# async def current_theme():
-#     theme_name = get_theme_name()
-#     return {"current_theme": theme_name}
-
 
 
 from fastapi import FastAPI, HTTPException
@@ -159,7 +52,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # Définition de la route pour obtenir un quiz par son code
-@app.get("/quiz/{code}")
+@app.post("/quiz/{code}")
 def get_quiz_by_code(code: str):
     db = SessionLocal()
     quiz = db.query(Quiz).filter(Quiz.code_quiz == code).all()
